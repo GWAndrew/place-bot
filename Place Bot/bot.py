a6cec918b94be102c8f4512dc4c8d6103f435bb6
GWAndrew <gwandrewgd@gmail.com>
2020-10-20 16:36:08 +0100

    bot update

--- Place Bot/bot.py
+++ Place Bot/bot.py
@@ -10,10 +10,10 @@ from PIL import Image, ImageDraw, ImageFilter,ImageFont
 from time import sleep
 import requests
 import shutil
+from bs4 import BeautifulSoup
 
 bot = commands.Bot(command_prefix="!!")
 bot.remove_command("help")
-path="C:/Users/gwand/Documents/Place Bot/"
 
 @bot.event
 async def on_ready():
@@ -345,8 +345,57 @@ async def unban(ctx, id: int):
         await ctx.send(embed=embed)
 
 @bot.command(pass_context=True)
-async def instagram(ctx, *, arg):
-    pass
-    #lol testd
+async def instagram(ctx, arg):
+    # instagram URL
+    URL = "https://www.instagram.com/{}/"
+
+    # parse function
+    def parse_data(s):
+
+        # creating a dictionary
+        data = {}
+
+        # splittting the content
+        # then taking the first part
+        s = s.split("-")[0]
+
+        # again splitting the content
+        s = s.split(" ")
+
+        # assigning the values
+        data['Followers'] = s[0]
+        data['Following'] = s[2]
+        data['Posts'] = s[4]
+
+        # returning the dictionary
+        return data
+
+    # scrape function
+    def scrape_data(username):
+
+        # getting the request from url
+        r = requests.get(URL.format(username))
+
+        # converting the text
+        s = BeautifulSoup(r.text, "html.parser")
+
+        # finding meta info
+        meta = s.find("meta", property ="og:description")
+
+        # calling parse method
+        return parse_data(meta.attrs['content'])
+
+    # main function
+    if __name__=="__main__":
+
+        # user name
+        username = str(arg)
+
+        # calling scrape function
+        data = scrape_data(username)
+
+        # printing the info
+        await ctx.send(str(data))
+        print(data)
 
 bot.run(token["token"])
