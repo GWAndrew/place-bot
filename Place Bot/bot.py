948944b00144236080a7fd43dcd6eb841f1c3079
GWAndrew <gwandrewgd@gmail.com>
2020-10-20 19:39:03 +0100

    bot update

--- Place Bot/bot.py
+++ Place Bot/bot.py
@@ -11,6 +11,7 @@ from time import sleep
 import requests
 import shutil
 from bs4 import BeautifulSoup
+from instagramy import InstagramUser
 
 bot = commands.Bot(command_prefix="!!")
 bot.remove_command("help")
@@ -346,59 +347,11 @@ async def unban(ctx, id: int):
 
 @bot.command(pass_context=True)
 async def instagram(ctx, arg):
-    # instagram URL
-    URL = "https://www.instagram.com/{}/"
-
-    # parse function
-    def parse_data(s):
-
-        # creating a dictionary
-        data = {}
-
-        # splittting the content
-        # then taking the first part
-        s = s.split("-")[0]
-
-        # again splitting the content
-        s = s.split(" ")
-
-        # assigning the values
-        data['Followers'] = s[0]
-        data['Following'] = s[2]
-        data['Posts'] = s[4]
-
-        # returning the dictionary
-        return data
-
-    # scrape function
-    def scrape_data(username):
-
-        # getting the request from url
-        r = requests.get(URL.format(username))
-
-        # converting the text
-        s = BeautifulSoup(r.text, "html.parser")
-
-        # finding meta info
-        meta = s.find("meta", property ="og:description")
-
-        # calling parse method
-        return parse_data(meta.attrs['content'])
-
-    # main function
-    if __name__=="__main__":
-
-        # user name
-        username = str(arg)
-
-        # calling scrape function
-        data = scrape_data(username)
-
-        # printing the info
-        await ctx.send(str(data))
-        print(data)
-
-
+    user = InstagramUser(str(arg))
+    #profile_pic = user.profile_pic_url
+    await ctx.send(user.is_verified)
+    await ctx.send(user.number_of_followers)
+    await ctx.send(user.number_of_posts)
 
 token = open("token.txt", "r")
 
